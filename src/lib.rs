c057a6338e02c3a4b46e449dd7a0f25b9c4ffd5c
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-09-06 16:50:35 +0300

    remove all dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,18 @@
 #[cfg(feature = "devnet")]
 pub mod constants {
-    use solana_program::pubkey::Pubkey;
-
     // staking dependencies
-    pub const REWARD_CONTRACT_ID: Pubkey =
-        solana_program::pubkey!("BF5PatmRTQDgEKoXR7iHRbkibEEi83nVM38cUKWzQcTR");
-    pub const DAO_PUBKEY: Pubkey =
-        solana_program::pubkey!("89wVNeyqqDaWKWtS4rbunYdsxxbe5V3VRx6g8GWNMTMt");
-    pub const GOVERNANCE_PROGRAM_ID: Pubkey =
-        solana_program::pubkey!("GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw");
+    pub const REWARD_CONTRACT_ID: &str = "BF5PatmRTQDgEKoXR7iHRbkibEEi83nVM38cUKWzQcTR";
+    pub const DAO_PUBKEY: &str = "89wVNeyqqDaWKWtS4rbunYdsxxbe5V3VRx6g8GWNMTMt";
+    pub const GOVERNANCE_PROGRAM_ID: &str = "GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw";
     pub const REALM_NAME: &str = "VSR Rewards 21";
 }
 
 #[cfg(feature = "mainnet")]
 pub mod constants {
     // staking dependencies
-    pub const REWARD_CONTRACT_ID: Pubkey = solana_program::pubkey!("");
-    pub const DAO_PUBKEY: Pubkey = solana_program::pubkey!("");
-    pub const GOVERNANCE_PROGRAM_ID: Pubkey = solana_program::pubkey!("");
+    // staking dependencies
+    pub const REWARD_CONTRACT_ID: &str = "";
+    pub const DAO_PUBKEY: &str = "";
+    pub const GOVERNANCE_PROGRAM_ID: &str = "";
     pub const REALM_NAME: &str = "";
 }
