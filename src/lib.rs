7260056b4299492bc8fb6bd7e3db3453e12ec462
Vadim <grozinok@gmail.com>
2024-09-16 13:02:23 +0200

    feat: add few more constants

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,11 @@ pub mod constants {
     // staking dependencies
     pub const REWARD_CONTRACT_ID: &str = "BF5PatmRTQDgEKoXR7iHRbkibEEi83nVM38cUKWzQcTR";
     pub const DAO_PUBKEY: &str = "89wVNeyqqDaWKWtS4rbunYdsxxbe5V3VRx6g8GWNMTMt";
+    // DAO's community mint
+    pub const DAO_GOVERNING_MINT: &str = "C83Ab2ZJ4nSFyVpSJXLkoPph4XVU6y2jgxJDczT4qddZ";
     pub const GOVERNANCE_PROGRAM_ID: &str = "GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw";
     pub const REALM_NAME: &str = "VSR Rewards 21";
+    pub const FEE_RECEIVER: &str = "EzsKaQq61FLZwRaiUx7t17LWVVzsE8wRkhBghFrZGGwG";
 }
 
 #[cfg(feature = "mainnet")]
@@ -12,6 +15,9 @@ pub mod constants {
     // staking dependencies
     pub const REWARD_CONTRACT_ID: &str = "";
     pub const DAO_PUBKEY: &str = "";
+    // DAO's community mint
+    pub const DAO_GOVERNING_MINT: &str = "";
     pub const GOVERNANCE_PROGRAM_ID: &str = "";
     pub const REALM_NAME: &str = "";
+    pub const FEE_RECEIVER: &str = "";
 }
