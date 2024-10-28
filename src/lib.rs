0595ff629b4b1ba574431657f411ce269f906e5e
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-10-28 13:43:36 +0200

    update mainnet addresses

--- src/lib.rs
+++ src/lib.rs
@@ -33,24 +33,34 @@ pub mod constants {
 #[cfg(feature = "mainnet")]
 pub mod constants {
     // staking dependencies
+
+    /// ID of the reward contract
+    /// DdAfv8RS2BS41FRjDX5nLXSmQWrPsdC17sbgD66oKcU8
     pub const REWARD_CONTRACT_ID: [u8; 32] = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0,
+        187, 144, 22, 85, 66, 234, 175, 207, 244, 245, 195, 176, 62, 198, 185, 87, 88, 123, 166,
+        137, 125, 120, 47, 188, 37, 228, 127, 164, 75, 100, 222, 33,
     ];
+
+    /// DAO address
+    /// DA5G7QQbFioZ6K33wQcH8fVdgFcnaDjLD7DLQkapZg5X
     pub const DAO_PUBKEY: [u8; 32] = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0,
+        180, 159, 73, 172, 121, 229, 6, 162, 178, 13, 8, 50, 165, 16, 121, 152, 154, 60, 119, 252,
+        84, 10, 227, 182, 105, 62, 225, 11, 189, 221, 7, 82,
     ];
-    // DAO's community mint
+    /// DAO's community mint
+    /// METAewgxyPbgwsseH8T16a39CQ5VyVxZi9zXiDPY18m
     pub const DAO_GOVERNING_MINT: [u8; 32] = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0,
+        5, 46, 211, 80, 16, 184, 25, 255, 73, 20, 244, 122, 49, 24, 196, 44, 152, 191, 33, 15, 215,
+        228, 125, 114, 35, 7, 181, 194, 73, 1, 167, 186,
     ];
+
+    /// The address of the Governance Program (Metaplex fork)
+    /// 4N9Es8WmcBfq9kdh7WYmoHExztRZMiA7WeAmARtpk3SF
     pub const GOVERNANCE_PROGRAM_ID: [u8; 32] = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0,
+        49, 253, 146, 220, 202, 187, 156, 50, 204, 249, 40, 183, 62, 33, 227, 109, 150, 87, 63,
+        217, 161, 102, 42, 216, 242, 63, 162, 98, 104, 242, 160, 8,
     ];
-    pub const REALM_NAME: &str = "";
+    pub const REALM_NAME: &str = "Metaplex DAO";
     pub const FEE_RECEIVER: [u8; 32] = [
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0,
